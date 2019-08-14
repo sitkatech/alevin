ca587e42eb893dbe08456caaf553ec3b8e79fe4d
raynardlee <raynardlee@yahoo.com>
2019-08-14 08:36:25 -0700

    [projectfirma/#1646] Create Project Index page EF ORM is codefirst, so we cannot add properties on the partial like we used to be able to do on a dbfirst project.  You need to move them into static extension classes or change them to function calls instead of properties

---
+++ Source/ProjectFirma.Web/Models/ReclamationAgreementModelExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using ProjectFirmaModels.Models;
+
+namespace ProjectFirma.Web.Models
+{
+    public static class ReclamationAgreementModelExtensions
+    {
+        /// <summary>
+        /// Convenience accessor for *SINGLE* Reclamation Cost Authorities.
+        /// Unsure if this is a safe assumption yet. If it is, we don't need ReclamationCostAuthorities above.
+        /// </summary>
+        public static ReclamationCostAuthority GetReclamationCostAuthority(this ReclamationAgreement reclamationAgreement)
+        {
+            //var blah2 = ReclamationAgreementReclamationCostAuthorities.ToList();
+
+            /*
+            var blah = ReclamationCostAuthorities;
+            var otherBlah = blah.SingleOrDefault();
+            return otherBlah;
+            */
+            // This crashes: Invalid column name 'ReclamationAgreement_ReclamationAgreementID'
+            return reclamationAgreement.ReclamationAgreementReclamationCostAuthorities.Select(rarca => rarca.ReclamationCostAuthority)
+                .SingleOrDefault();
+        }
+
+    }
+}
--- Source/ProjectFirmaModels/Models/ReclamationAgreement.cs
+++ Source/ProjectFirmaModels/Models/ReclamationAgreement.cs
@@ -9,38 +9,5 @@ namespace ProjectFirmaModels.Models
         {
             return $"ReclamationAgreement: {this.ReclamationAgreementID} - {this.AgreementNumber}";
         }
-
-        /// <summary>
-        /// Convenience accessor for Reclamation Cost Authorities
-        /// </summary>
-        public List<ReclamationCostAuthority> ReclamationCostAuthorities
-        {
-            get
-            {
-                return this.ReclamationAgreementReclamationCostAuthorities.Select(rarca => rarca.ReclamationCostAuthority).ToList();
-            }
-        }
-
-        /// <summary>
-        /// Convenience accessor for *SINGLE* Reclamation Cost Authorities.
-        /// Unsure if this is a safe assumption yet. If it is, we don't need ReclamationCostAuthorities above.
-        /// </summary>
-        public ReclamationCostAuthority ReclamationCostAuthority
-        {
-            get
-            {
-                //var blah2 = ReclamationAgreementReclamationCostAuthorities.ToList();
-
-                /*
-                var blah = ReclamationCostAuthorities;
-                var otherBlah = blah.SingleOrDefault();
-                return otherBlah;
-                */
-                // This crashes: Invalid column name 'ReclamationAgreement_ReclamationAgreementID'
-                return this.ReclamationAgreementReclamationCostAuthorities.Select(rarca => rarca.ReclamationCostAuthority).SingleOrDefault();
-            }
-        }
-
-
     }
 }
