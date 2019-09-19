35928d0a65a5c06e032589f4fb667ea9142c106f
tomkamin <tom.kamin@sitkatech.com>
2019-09-18 18:13:52 -0700

    [projectfirma/#1740] Associate Reclamation's Habitat Project to a HUC adding new temp table script

---
+++ Database/Tables/dbo.tmpHuc12.sql
@@ -0,0 +1,29 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tmpHuc12](
+	[OBJECTID_1] [int] NULL,
+	[OBJECTID] [float] NULL,
+	[TNMID] [varchar](40) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[MetaSource] [varchar](40) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[SourceData] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[SourceOrig] [varchar](130) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[SourceFeat] [varchar](40) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[LoadDate] [varchar](24) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[NonContrib] [float] NULL,
+	[NonContr_1] [float] NULL,
+	[AreaSqKm] [float] NULL,
+	[AreaAcres] [float] NULL,
+	[GNIS_ID] [int] NULL,
+	[Name] [varchar](120) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[States] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[HUC12] [varchar](12) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[HUType] [varchar](254) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[HUMod] [varchar](30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[ToHUC] [varchar](16) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Shape_Leng] [float] NULL,
+	[Shape_Length] [float] NULL,
+	[Shape_Area] [float] NULL,
+	[GEOM] [geometry] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
