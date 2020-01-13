a61c4da1e48c27afd3699a0dee7c4f04becfe0c0
stewartgordonsitka <stewart.gordon@sitkatech.com>
2020-01-13 15:21:07 -0800

    [projectfirma/#2012] Capture Processing Data on an Agreement Request * Add new section to AgreementDetail page * Add modal dialog to edit the Requisition Information on an agreement

---
+++ Source/ProjectFirma.Web/Views/AgreementRequest/EditRequisitionInformationViewData.cs
@@ -0,0 +1,11 @@
+﻿using ProjectFirmaModels.Models;
+
+namespace ProjectFirma.Web.Views.AgreementRequest
+{
+    public class EditRequisitionInformationViewData : FirmaViewData
+    {
+        public EditRequisitionInformationViewData(FirmaSession currentFirmaSession) : base(currentFirmaSession)
+        {
+        }
+    }
+}
