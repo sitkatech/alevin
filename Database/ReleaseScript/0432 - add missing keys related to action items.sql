52a9aa9c4fa385e16f7c573c7f78ffbfc10b4be5
stewartgordonsitka <stewart.gordon@sitkatech.com>
2020-01-27 13:55:29 -0800

    [projectfirma/#2039] Assign Action Items for a Project * Add missing keys related to action items after merging in from PF

---
+++ Database/ReleaseScript/0432 - add missing keys related to action items.sql
@@ -0,0 +1,8 @@
+alter table dbo.ActionItem add constraint FK_ActionItem_Project_ProjectID_TenantID foreign key (ProjectID, TenantID) references dbo.Project(ProjectID, TenantID)
+go
+
+alter table dbo.ActionItem add constraint FK_ActionItem_Person_AssignedToPersonID_TenantID foreign key (AssignedToPersonID, TenantID) references dbo.Person(PersonID, TenantID)
+go
+
+alter table dbo.ActionItem add constraint FK_ActionItem_ProjectProjectStatus_ProjectProjectStatusID_TenantID foreign key (ProjectProjectStatusID, TenantID) references dbo.ProjectProjectStatus(ProjectProjectStatusID, TenantID)
+go
