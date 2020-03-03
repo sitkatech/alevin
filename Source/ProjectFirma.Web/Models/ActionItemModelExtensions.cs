86607da2793ca57ed4d9e536477d4df2e4f4ecde
stewartgordonsitka <stewart.gordon@sitkatech.com>
2020-03-03 09:44:26 -0800

    [projectfirma/#2065] Link to associated Action Items in Status Updates * Adding model extensions on the action items for some simple htmlstrings * Creating and styling the action item for the project update statuses

--- Source/ProjectFirma.Web/Models/ActionItemModelExtensions.cs
+++ Source/ProjectFirma.Web/Models/ActionItemModelExtensions.cs
@@ -1,6 +1,10 @@
-﻿using ProjectFirma.Web.Common;
+﻿using LtInfo.Common;
+using LtInfo.Common.DhtmlWrappers;
+using LtInfo.Common.ModalDialog;
+using ProjectFirma.Web.Common;
 using ProjectFirma.Web.Controllers;
 using ProjectFirmaModels.Models;
+using System.Web;
 
 namespace ProjectFirma.Web.Models
 {
@@ -15,5 +19,44 @@ namespace ProjectFirma.Web.Models
         {
             return SitkaRoute<ActionItemController>.BuildUrlFromExpression(c => c.Edit(actionItem.ActionItemID));
         }
+
+        public static HtmlString GetDisplayHeaderHtmlString(this ActionItem actionItem, bool userHasEditPermissions)
+        {
+            var editButtonString = actionItem.GetEditButtonHtmlString(userHasEditPermissions);
+            var returnString = $"{editButtonString} {actionItem.ActionItemText.Truncate(75, "...")}".Trim();
+            return new HtmlString(returnString);
+        }
+
+        public static HtmlString GetDisplayFooterHtmlString(this ActionItem actionItem)
+        {
+            return new HtmlString($"{actionItem.GetStatusWithIconHtmlString()} {actionItem.GetAssignedToPersonWithIconHtmlString()} {actionItem.GetDueDateWithIconHtmlString()}");
+        }
+
+        public static HtmlString GetAssignedToPersonWithIconHtmlString(this ActionItem actionItem)
+        {
+            return new HtmlString($"<span title=\"{FieldDefinitionEnum.ActionItemAssignedToPerson.ToType().FieldDefinitionDisplayName}\"><span class=\"glyphicon glyphicon-user\" aria-hidden=\"true\"></span> {actionItem.AssignedToPerson.GetFullNameFirstLast()}</span>");
+        }
+
+        public static HtmlString GetDueDateWithIconHtmlString(this ActionItem actionItem)
+        {
+            return new HtmlString($"<span title=\"{FieldDefinitionEnum.ActionItemDueByDate.ToType().FieldDefinitionDisplayName}\"><span class=\"glyphicon glyphicon-calendar\" aria-hidden=\"true\"></span> {actionItem.DueByDate.ToShortDateString()}</span>");
+        }
+
+        public static HtmlString GetEditButtonHtmlString(this ActionItem actionItem, bool userHasEditPermissions)
+        {
+            var buttonString = userHasEditPermissions 
+                ? $"{DhtmlxGridHtmlHelpers.MakeEditIconAsModalDialogLinkBootstrap(new ModalDialogForm(actionItem.GetEditUrl(), ModalDialogFormHelper.DefaultDialogWidth, "Edit Action Item"))}" 
+                : string.Empty;
+            return new HtmlString(buttonString);
+        }
+
+        public static HtmlString GetStatusWithIconHtmlString(this ActionItem actionItem)
+        {
+            var glyphiconClass = actionItem.ActionItemState.ToEnum == ActionItemStateEnum.Complete
+                ? "glyphicon-ok text-success"
+                : "glyphicon-remove text-danger";
+            return new HtmlString($"<span title=\"{FieldDefinitionEnum.ActionItemState.ToType().FieldDefinitionDisplayName}\"><span class=\"glyphicon {glyphiconClass}\" aria-hidden=\"true\"></span> {actionItem.ActionItemState.ActionItemStateDisplayName}</span>");
+        }
+
     }
 }
