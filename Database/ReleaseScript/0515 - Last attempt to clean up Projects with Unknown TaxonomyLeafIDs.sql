5f7301d3308e7a1d7afdd684b0931ebac51b050a
StewLGSitka <stewart@sitkatech.com>
2020-04-06 10:14:58 -0700

    [projectfirma/#2034] Assign Projects to Taxonomy by CAWBS This is a placeholder in case we get suggestions about where to put these projects.

---
+++ Database/ReleaseScript/0515 - Last attempt to clean up Projects with Unknown TaxonomyLeafIDs.sql
@@ -0,0 +1,10 @@
+--What's left? These are projects which are still associated with the 'Unknown xx.xxxxx' category. Some of these can definitely be moved.
+
+/*
+
+select p.*
+from dbo.Project as p
+where p.TenantID = 12
+and p.TaxonomyLeafID = 2680
+
+*/
