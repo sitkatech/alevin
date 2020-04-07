05ba97b65127e6c45927e38191d57ecbcfc43fe0
StewLGSitka <stewart@sitkatech.com>
2020-04-07 15:53:10 -0700

    Getting BOC groupings imported in the raw, ready to create our Reclamataion schema version of it.

---
+++ Database/ReleaseScript/0518 - Adding BudgetObjectCodeGroup structure.sql
@@ -0,0 +1,27 @@
+--select * from [ReclamationOneTimeImport].[BocCleaned]
+--select * from Reclamation.BudgetObjectCode
+
+--begin tran
+
+CREATE TABLE Reclamation.BudgetObjectCodeGroup
+(
+    BudgetObjectCodeGroupID [int] IDENTITY(1,1) NOT NULL,
+    -- '10', '11', '11.1', etc.
+    BudgetObjectCodeGroupPrefix nvarchar(5) not null,
+    BudgetObjectCodeGroupName [nvarchar](100) not NULL,
+    BudgetObjectCodeGroupDefinition [nvarchar](500) null
+ CONSTRAINT [PK_BudgetObjectCodeGroup_BudgetObjectCodeGroupID] PRIMARY KEY CLUSTERED 
+ (
+    BudgetObjectCodeGroupID ASC
+ ) ON [PRIMARY]
+)
+GO
+
+ALTER TABLE Reclamation.BudgetObjectCodeGroup
+ADD CONSTRAINT UC_BudgetObjectCodeGroup_BudgetObjectCodeGroupPrefix UNIQUE (BudgetObjectCodeGroupPrefix); 
+
+
+ALTER TABLE Reclamation.BudgetObjectCodeGroup
+ADD CONSTRAINT UC_BudgetObjectCodeGroup_BudgetObjectCodeGroupName UNIQUE (BudgetObjectCodeGroupName); 
+
+--rollback tran
