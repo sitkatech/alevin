7fd92b1c6895107da6dc4dd0111ffd88d4d77b3f
StewLGSitka <stewart@sitkatech.com>
2020-05-05 15:58:28 -0700

    [projectfirma/#2108] Integrate PnBudget Financials into Alevin ETL * Adding rest of columns to PnBudgetGridSpec

--- Source/ProjectFirma.Web/Views/PnBudget/PnBudgetGridSpec.cs
+++ Source/ProjectFirma.Web/Views/PnBudget/PnBudgetGridSpec.cs
@@ -20,7 +20,19 @@ namespace ProjectFirma.Web.Views.PnBudget
             //Add(FieldDefinitionEnum.PnBudget.ToType().ToGridHeaderString(), ob => UrlTemplate.MakeHrefString(ob?.GetDetailUrl(), ob?.ObligationNumberKey), 100, DhtmlxGridColumnFilterType.Text);
             Add(FieldDefinitionEnum.PnBudget.ToType().ToGridHeaderString(), pnb => pnb.WbsElementPnBudgetID.ToString(), 100, DhtmlxGridColumnFilterType.Text);
             Add("Cost Authority", pnb => pnb.CostAuthority?.GetDetailLinkUsingCostAuthorityWorkBreakdownStructure(), 150, DhtmlxGridColumnFilterType.Html);
-            Add("Fund Type", pnb => pnb.PnBudgetFundType.PnBudgetFundTypeDisplayName, 100, DhtmlxGridColumnFilterType.SelectFilterStrict);
+            Add("Fund Type", pnb => pnb.PnBudgetFundType.PnBudgetFundTypeDisplayName, 125, DhtmlxGridColumnFilterType.SelectFilterStrict);
+            Add("Funding Source", pnb => pnb.FundingSource.FundingSourceName, 100, DhtmlxGridColumnFilterType.SelectFilterStrict);
+            Add("Funds Center", pnb => pnb.FundsCenter, 100, DhtmlxGridColumnFilterType.Text);
+            Add("Fiscal Quarter", pnb => pnb.FiscalQuarter.FiscalQuarterDisplayName, 125, DhtmlxGridColumnFilterType.SelectFilterStrict);
+            Add("Fiscal Year", pnb => pnb.FiscalYear.ToString(), 50, DhtmlxGridColumnFilterType.SelectFilterStrict);
+            Add("Commitment Item", pnb => pnb.CommitmentItem.CommitmentItemName, 75, DhtmlxGridColumnFilterType.SelectFilterStrict);
+            Add("FIDoc Number", pnb => pnb.FIDocNumber, 75, DhtmlxGridColumnFilterType.Text);
+            Add("Recoveries", pnb => pnb.Recoveries.ToStringCurrency(), 100, DhtmlxGridColumnFilterType.Numeric);
+            Add("Committed But Not Obligated", pnb => pnb.CommittedButNotObligated.ToStringCurrency(), 100, DhtmlxGridColumnFilterType.Numeric);
+            Add("Total Obligations", pnb => pnb.TotalObligations.ToStringCurrency(), 100, DhtmlxGridColumnFilterType.Numeric);
+            Add("Total Expenditures", pnb => pnb.TotalExpenditures.ToStringCurrency(), 100, DhtmlxGridColumnFilterType.Numeric);
+            Add("Undelivered Orders", pnb => pnb.UndeliveredOrders.ToStringCurrency(), 100, DhtmlxGridColumnFilterType.Numeric);
+            
         }
     }
 }
