3c53ef50af208f1d037586ca8e1a3b1a340025d9
tomkamin <tom.kamin@sitkatech.com>
2020-08-18 17:48:34 -0700

    Adding test to ensure the TaxonomyLeaf call on a project does not crash

---
+++ Source/ProjectFirmaModels/Models/ProjectTaxonomyLeafTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using ProjectFirmaModels.UnitTestCommon;
+
+namespace ProjectFirmaModels.Models
+{
+    [TestFixture]
+    public class ProjectTaxonomyLeafTest
+    {
+        [Test]
+        public void EnsureAllTaxonomyLeavesDontCrash()
+        {
+            var projects = HttpRequestStorageForTest.DatabaseEntities.Projects.ToList();
+            //This can crash when data is garbled. This test is to make sure this runs for all projects.
+            projects.ForEach(p => p.GetTaxonomyLeaf());
+            
+        }
+
+    }
+}
