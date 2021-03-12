b45776750b676553fb6b687c427313e4e07ace3d
StewLGSitka <stewart@sitkatech.com>
2021-03-12 12:16:02 -0800

    Alevin: Mike worrying about even more cleanup.

--- Database/ReleaseScript/0672 - Cleaning Up Last Bits Of non-reclamation data.sql
+++ Database/ReleaseScript/0672 - Cleaning Up Last Bits Of non-reclamation data.sql
@@ -3,7 +3,7 @@
 update dbo.Organization
 set PrimaryContactPersonID = null,
     LogoFileResourceInfoID = null
-where (TenantID != 1 and TenantID != 12)
+where TenantID not in (12)
 
 /*
 select * from dbo.TenantAttribute
@@ -16,37 +16,53 @@ set TenantSquareLogoFileResourceInfoID = null,
     TenantStyleSheetFileResourceInfoID = null,
     TenantFactSheetLogoFileResourceInfoID = null,
     PrimaryContactPersonID = null
-where (TenantID != 1 and TenantID != 12)
+where TenantID not in (12)
 
 --select * from dbo.FileResourceInfo
 --where (TenantID != 1 and TenantID != 12)
 
 delete from dbo.FileResourceInfo
-where (TenantID != 1 and TenantID != 12)
+where TenantID not in (12)
 
 delete from dbo.Person
-where (TenantID != 1 and TenantID != 12)
+where TenantID not in (12)
 
 delete from dbo.Organization
-where (TenantID != 1 and TenantID != 12)
+where TenantID not in (12)
 
 delete from dbo.FirmaPage
-where (TenantID != 1 and TenantID != 12)
+where TenantID not in (12)
 
 delete from dbo.TenantAttribute
-where (TenantID != 1 and TenantID != 12)
+where TenantID not in (12)
 
 delete from dbo.OrganizationType
-where (TenantID != 1 and TenantID != 12)
+where TenantID not in (12)
 
 -- Make sure we've disabled other tenants
 update dbo.Tenant
 set TenantEnabled = 0
-where (TenantID != 1 and TenantID != 12)
+where TenantID not in (12)
 
 -- I think this could cause problems in C# at runtime, but it will execute if uncommented - everything else 
 -- is cleaned up.
-/*
-delete from dbo.Tenant
-where (TenantID != 1 and TenantID != 12)
-*/
+
+update dbo.Tenant
+set CanonicalHostNameLocal = 'local.xxx',
+    CanonicalHostNameQa = 'qa.xxx',
+    CanonicalHostNameProd = 'prod.xxx',
+    --UseFiscalYears = 0,
+    --UsesTechnicalAssistanceParameters = 0,
+    --ArePerformanceMeasuresExternallySourced = 0,
+    --AreOrganizationsExternallySourced = 0,
+    --AreFundingSourcesExternallySourced = 0,
+    TenantEnabled = 0
+where TenantID not in (12)
+
+--select * from dbo.Tenant
+
+--delete from dbo.Tenant
+--where (TenantID != 12)
+
+
+
