99e01a5a074dd4ff49537f496069426a9b1861b7
tomkamin <tom.kamin@sitkatech.com>
2021-04-21 17:58:30 -0700

    adding if exists check to PK rename

--- Database/ReleaseScript/0685 - renaming PK on LastSQLServerDatabaseBackup table.sql
+++ Database/ReleaseScript/0685 - renaming PK on LastSQLServerDatabaseBackup table.sql
@@ -1,1 +1,4 @@
-exec sp_rename 'dbo.PK_LastSQLServerDatabaseBackup', 'PK_LastSQLServerDatabaseBackup_LastSQLServerDatabaseBackupID', 'OBJECT'
+
+if exists (select * from INFORMATION_SCHEMA.TABLE_CONSTRAINTS where CONSTRAINT_NAME = 'PK_LastSQLServerDatabaseBackup') begin
+    exec sp_rename 'dbo.PK_LastSQLServerDatabaseBackup', 'PK_LastSQLServerDatabaseBackup_LastSQLServerDatabaseBackupID', 'OBJECT'
+end
