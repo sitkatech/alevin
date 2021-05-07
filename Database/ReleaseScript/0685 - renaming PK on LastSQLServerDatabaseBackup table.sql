9b82a522e99040c56770522640e01453789e2458
tomkamin <tom.kamin@sitkatech.com>
2021-05-06 18:09:00 -0700

    removing old release scripts

--- Database/ReleaseScript/0685 - renaming PK on LastSQLServerDatabaseBackup table.sql
+++
@@ -1,4 +0,0 @@
-
-if exists (select * from INFORMATION_SCHEMA.TABLE_CONSTRAINTS where CONSTRAINT_NAME = 'PK_LastSQLServerDatabaseBackup') begin
-    exec sp_rename 'dbo.PK_LastSQLServerDatabaseBackup', 'PK_LastSQLServerDatabaseBackup_LastSQLServerDatabaseBackupID', 'OBJECT'
-end
