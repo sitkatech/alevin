3e59fc3c4e5a78811291300b481a1a15462a4217
tomkamin <tom.kamin@sitkatech.com>
2021-05-25 11:30:14 -0700

    merge mainline. regen code. some drift from SQL 2019 upgrade

--- Database/Tables/Reclamation.CostAuthorityObligationRequestPotentialObligationNumberMatch.sql
+++ Database/Tables/Reclamation.CostAuthorityObligationRequestPotentialObligationNumberMatch.sql
@@ -9,12 +9,12 @@ CREATE TABLE [Reclamation].[CostAuthorityObligationRequestPotentialObligationNum
  CONSTRAINT [PK_CostAuthorityObligationRequestPotentialObligationNumberMatch_CostAuthorityObligationRequestPotentialObligationNumberMatchID] PRIMARY KEY CLUSTERED 
 (
 	[CostAuthorityObligationRequestPotentialObligationNumberMatchID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
  CONSTRAINT [AK_CostAuthorityObligationRequestPotentialObligationNumberMatch_CostAuthorityObligationRequestID_ObligationNumberID] UNIQUE NONCLUSTERED 
 (
 	[CostAuthorityObligationRequestID] ASC,
 	[ObligationNumberID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
