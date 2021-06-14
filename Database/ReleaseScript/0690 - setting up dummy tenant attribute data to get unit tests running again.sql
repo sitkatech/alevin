04adfe9b71735b66fd342263aa5b1882c0affaae
tomkamin <tom.kamin@sitkatech.com>
2021-06-14 09:28:48 -0700

    inserting dummy data to get tests running updating some tests to use Reclamation tenant instead of Sitka due to no orgs in DB for other tenants except Reclamation

--- Database/ReleaseScript/0690 - setting up dummy tenant attribute data to get unit tests running again.sql
+++ Database/ReleaseScript/0690 - setting up dummy tenant attribute data to get unit tests running again.sql
@@ -44,3 +44,40 @@ INSERT INTO [dbo].[TenantAttribute]
            (1,0xE61000000104050000000100000040285FC06911DAA3DB1C47400100000040285FC08D97B8A52102454001000000B0415DC08D97B8A52102454001000000B0415DC06911DAA3DB1C47400100000040285FC06911DAA3DB1C474001000000020000000001000000FFFFFFFF0000000003,2017,NULL,NULL,NULL,NULL,'ProjectFirma','ProjectFirma',1,2,2,1,0,1,NULL,NULL,NULL,1,0,1,NULL,0,'SitkaProjectFirma','6C0D5ACB-EF45-4081-AFDA-754DA37A87BD',2,0,0,NULL,1,'ProjectFirma',0,0,0,NULL,0,0,0,0,1)
 GO
 
+
+
+USE [AlevinDB]
+GO
+
+INSERT INTO [dbo].[ProjectUpdateSetting]
+           ([TenantID]
+           ,[ProjectUpdateKickOffDate]
+           ,[ProjectUpdateCloseOutDate]
+           ,[ProjectUpdateReminderInterval]
+           ,[EnableProjectUpdateReminders]
+           ,[SendPeriodicReminders]
+           ,[SendCloseOutNotification]
+           ,[ProjectUpdateKickOffIntroContent]
+           ,[ProjectUpdateReminderIntroContent]
+           ,[ProjectUpdateCloseOutIntroContent]
+           ,[DaysBeforeCloseOutDateForReminder])
+     VALUES
+           (1
+           ,null
+           ,null
+           ,null
+           ,0
+           ,0
+           ,0
+           ,null
+           ,null
+           ,null
+           ,null)
+GO
+
+
+
+
+
+
+
