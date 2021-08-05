8392dcbea01a430ecd7478a07ff2021cf55cdf97
tomkamin <tom.kamin@sitkatech.com>
2021-08-04 18:12:37 -0700

    PF-2387 Expand local authentication login functionality added ability for admin to create a new user account

---
+++ Source/ProjectFirma.Web/Views/User/CreateAccountViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using LtInfo.Common.Models;
+
+namespace ProjectFirma.Web.Views.User
+{
+    public class CreateAccountViewModel : FormViewModel
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [PasswordPropertyText]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("Organization")]
+        public int OrganizationID { get; set; }
+
+
+    }
+}
