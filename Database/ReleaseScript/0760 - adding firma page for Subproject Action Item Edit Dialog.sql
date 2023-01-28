6d0af7dddc289fbc56559385f1c8c746cac9c12a
tomkamin <tom.kamin@sitkatech.com>
2023-01-27 16:53:58 -0800

    PF-2517 Add Project Timeline to Sub Projects adding subproject action items edit dialog firma page

--- Database/ReleaseScript/0760 - adding firma page for Subproject Action Item Edit Dialog.sql
+++ Database/ReleaseScript/0760 - adding firma page for Subproject Action Item Edit Dialog.sql
@@ -1,4 +1,6 @@
-
+insert into dbo.FirmaPageType(FirmaPageTypeID, FirmaPageTypeName, FirmaPageTypeDisplayName, FirmaPageRenderTypeID)
+values
+(10014, 'SubprojectActionItemEditDialog', 'Subproject Action Item Edit Dialog', 2)
 
 insert into dbo.FirmaPage (FirmaPageTypeID, TenantID,  FirmaPageContent)
 values( 10014, 12, '')
