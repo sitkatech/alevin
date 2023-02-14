c49cae77a9a254a292ce15caac1ba5a6c99495c8
tomkamin <tom.kamin@sitkatech.com>
2023-02-13 19:24:49 -0800

    PF-2539 Add Obligations and Expenditures grid to Subbasin detail page getting grid onto the detail page and adding the needed columns

--- Source/ProjectFirma.Web/Views/GeospatialArea/FinancialDataByProjectAndCawbsForGeospatialAreaGridSpec.cs
+++ Source/ProjectFirma.Web/Views/GeospatialArea/FinancialDataByProjectAndCawbsForGeospatialAreaGridSpec.cs
@@ -1,6 +1,7 @@
 ﻿using LtInfo.Common;
 using LtInfo.Common.DhtmlWrappers;
 using LtInfo.Common.Views;
+using ProjectFirma.Web.Common;
 using ProjectFirma.Web.Models;
 using ProjectFirmaModels.Models;
 
@@ -13,7 +14,14 @@ namespace ProjectFirma.Web.Views.GeospatialArea
             //Add(geospatialAreaType.GeospatialAreaTypeName, a => UrlTemplate.MakeHrefString(GeospatialAreaModelExtensions.GetDetailUrl(a.GeospatialAreaID), a.GetGeospatialAreaShortNameWithColor()), 300, DhtmlxGridColumnFilterType.Html);
             //Add($"# of {FieldDefinitionEnum.Project.ToType().GetFieldDefinitionLabelPluralized()}", a => a.ProjectViewableByUserCount, 65);
 
-            Add("Project Name", x => x.Project.ProjectName, 180, DhtmlxGridColumnFilterType.SelectFilterHtmlStrict);
+            Add(FieldDefinitionEnum.ProjectName.ToType().ToGridHeaderString(), x => UrlTemplate.MakeHrefString(x.Project.GetDetailUrl(), x.Project.ProjectName), 300, DhtmlxGridColumnFilterType.SelectFilterHtmlStrict);
+            Add(FieldDefinitionEnum.CostAuthorityWorkBreakdownStructure.ToType().ToGridHeaderString(),
+                x => x.CostAuthority.GetDetailLinkUsingCostAuthorityWorkBreakdownStructure(), 200,
+                DhtmlxGridColumnFilterType.SelectFilterHtmlStrict);
+            Add("Fiscal Year", x => x.WbsElementPnBudget.FiscalYear.ToString(), 80, DhtmlxGridColumnFilterType.SelectFilterStrict);
+
+            Add(FieldDefinitionEnum.Obligation.ToType().ToGridHeaderString(), x => x.WbsElementPnBudget.TotalObligations, 100, DhtmlxGridColumnFormatType.Decimal, DhtmlxGridColumnAggregationType.Total);
+            Add(FieldDefinitionEnum.TotalExpenditures.ToType().ToGridHeaderString(), x => x.WbsElementPnBudget.TotalExpenditures, 100, DhtmlxGridColumnFormatType.Decimal, DhtmlxGridColumnAggregationType.Total);
         }
     }
 }
