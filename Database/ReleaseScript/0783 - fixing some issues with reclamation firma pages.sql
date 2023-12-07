e9a59018a6268911e711c0315fee47ea0c25e8cf
tomkamin <tom.kamin@sitkatech.com>
2023-12-06 19:15:56 -0800

    fixing issues with build and unit tests

--- Database/ReleaseScript/0783 - fixing some issues with reclamation firma pages.sql
+++ Database/ReleaseScript/0783 - fixing some issues with reclamation firma pages.sql
@@ -13,3 +13,12 @@ values
 insert into dbo.FirmaPage(TenantID, FirmaPageTypeID) values (12,91) --Could Not find Firma Page Type 'ProgressDashboardIntro'(91) in Firma Pages for Tenant BureauOfReclamation(12)
 insert into dbo.FirmaPage(TenantID, FirmaPageTypeID) values (12,92) --Could Not find Firma Page Type 'ProgressDashboardAcresConstructedByTheNumbers'(92) in Firma Pages for Tenant BureauOfReclamation(12)
 insert into dbo.FirmaPage(TenantID, FirmaPageTypeID) values (12,93) --Could Not find Firma Page Type 'ProgressDashboardAcresConstructedPieCharts'(93) in Firma Pages for Tenant BureauOfReclamation(12)
+
+
+
+
+
+exec sp_rename 'dbo.FK__ExternalM__MapLe__3A8349D5', 'FK_ExternalMapLayer_FileResourceInfo_MapLegendImageFileResourceInfoID_FileResourceInfoID', 'OBJECT'
+exec sp_rename 'dbo.FK_ExternalMapLayer_FileResourceInfo_MapLegendImageFileResourceInfoID_TenantID', 'FK_ExternalMapLayer_FileResourceInfo_MapLegendImageFileResourceInfoID_TenantID_FileResourceInfoID_TenantID', 'OBJECT'
+exec sp_rename 'dbo.FK__Geospatia__MapLe__3B776E0E', 'FK_GeospatialAreaType_FileResourceInfo_MapLegendImageFileResourceInfoID_FileResourceInfoID', 'OBJECT'
+exec sp_rename 'dbo.FK_GeospatialAreaType_FileResourceInfo_MapLegendImageFileResourceInfoID_TenantID', 'FK_GeospatialAreaType_FileResourceInfo_MapLegendImageFileResourceInfoID_TenantID_FileResourceInfoID_TenantID', 'OBJECT'
