228b4deee1576e6d413592a16cbd2b3915af1b9a
tomkamin <tom.kamin@sitkatech.com>
2024-12-06 18:07:38 -0800

    PF-2734 Errors uploading new FBMS data updated error check

--- Database/Objects/Procs/dbo.pReclamationImportUnexpendedBalancePayRecV3.sql
+++ Database/Objects/Procs/dbo.pReclamationImportUnexpendedBalancePayRecV3.sql
@@ -59,10 +59,10 @@ end
 --414883	R000	R1678	R16780000.000000	R1024F0032	10	24XR0680G3	RX.16786911.1000000	CAUC & COORD    CAUCUS COORD ACTIVITY	252A	70427047	INTER-FLUVE, INC.	NULL	0
 -- TK 12/05/2024 - After talking with Noah and Jeremy at Reclamation, it sounds like there may be 2 more records coming in January 2025 and a couple of these are of no concern, but they thought it was a good idea to leave this check in place in case there are 8 or more with this issue.
 declare @nullSplDateCount int
-set @nullSplDateCount = (select count(*) from ImportFinancial.ImportFinancialImpPayRecUnexpendedV3 as pr where PostingDatePerSpl is null)
-if (@nullSplDateCount >= 8 )
+set @nullSplDateCount = (select count(*) from ImportFinancial.ImportFinancialImpPayRecUnexpendedV3 as pr where PostingDatePerSpl is null and UnexpendedBalance > 0)
+if (@nullSplDateCount > 0)
 begin
-   raiserror('Expected less than 8 PostingDatePerSpl in ImportFinancial.ImportFinancialImpPayRecUnexpendedV3 with null values.', 16,1)
+   raiserror('Expected no records with a null PostingDatePerSpl and a value greater than 0 in UexpendedBalance in ImportFinancial.ImportFinancialImpPayRecUnexpendedV3. But found some.', 16,1)
    return -1
 end
 
